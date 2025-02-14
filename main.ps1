132eed8c147dbf95ff935bcede6d1b20b8edebcc
apaza610 <homar.orozco@gmail.com>
2025-02-14 15:38:04 -0400

    added options for compress, resize, speed

--- main.ps1
+++ main.ps1
@@ -1,41 +1,84 @@
 Add-Type -AssemblyName System.Windows.Forms
 
 $miFormulario = New-Object System.Windows.Forms.Form
-$miFormulario.Text = "Compresor de videos"
-$miFormulario.Size = New-Object System.Drawing.Size(300,190)
+$miFormulario.Text = "CompresorDeVideos"
+$miFormulario.Size = New-Object System.Drawing.Size(300,200)
 
-$miLabel = New-Object System.Windows.Forms.Label
-$miLabel.Location = New-Object System.Drawing.Point(10,10)
-$miLabel.Size = New-Object System.Drawing.Size(200,20)
-$miLabel.Text = "Carpeta de videos a editar"
+$labelFolderPath = New-Object System.Windows.Forms.Label
+$labelFolderPath.Location = New-Object System.Drawing.Point(10,10)
+$labelFolderPath.Size = New-Object System.Drawing.Size(63,20)
+$labelFolderPath.Text = "FolderPath"
 
-$miCajaTexto = New-Object System.Windows.Forms.TextBox
-$miCajaTexto.Location = New-Object System.Drawing.Point(10,30)
-$miCajaTexto.Size = New-Object System.Drawing.Size(200,20)
+$textboxFolderPath = New-Object System.Windows.Forms.TextBox
+$textboxFolderPath.Location = New-Object System.Drawing.Point(80,10)
+$textboxFolderPath.Size = New-Object System.Drawing.Size(180,20)
 
-$miGroupBox = New-Object System.Windows.Forms.GroupBox
-$radioButtonCompresion = New-Object System.Windows.Forms.RadioButton
-$radioButtonCompresion.Text = "Comprime"
-$radioButtonCompresion.Checked = $true
-$radioButtonRedimension = New-Object System.Windows.Forms.RadioButton
-$radioButtonRedimension.Text = "Resize+Compr"
-$miGroupBox.Controls.AddRange(($radioButtonCompresion, $radioButtonRedimension))
-$miGroupBox.Location = New-Object System.Drawing.Point(10,60)
-$radioButtonCompresion.Location = New-Object System.Drawing.Point(10,10)
-$radioButtonRedimension.Location = New-Object System.Drawing.Point(115,10)
-$miGroupBox.Size = New-Object System.Drawing.Size(260,40)
+#----------------------------------------------------------------------
+$groupbox2 = New-Object System.Windows.Forms.GroupBox
+
+$checkboxCompress = New-Object System.Windows.Forms.CheckBox
+$checkboxCompress.Text = "Compress"
+$checkboxCompress.Checked = $true
+$checkboxCompress.Location = New-Object System.Drawing.Point(10,10)
+$checkboxCompress.Size = New-Object System.Drawing.Size(80,20)
+
+$checkboxResize = New-Object System.Windows.Forms.CheckBox
+$checkboxResize.Text = "Resize"
+$checkboxResize.Location = New-Object System.Drawing.Point(100,10)
+$checkboxResize.Size = New-Object System.Drawing.Size(80,20)
+
+$checkboxSpeed = New-Object System.Windows.Forms.CheckBox
+$checkboxSpeed.Text = "Speed"
+$checkboxSpeed.Location = New-Object System.Drawing.Point(180,10)
+$checkboxSpeed.Size = New-Object System.Drawing.Size(80,20)
+
+$groupbox2.Controls.AddRange($checkboxCompress, $checkboxResize, $checkboxSpeed)
+$groupbox2.Location = New-Object System.Drawing.Point(10,40)
+$groupbox2.Size = New-Object System.Drawing.Size(260,40)
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+$groupbox3 = New-Object System.Windows.Forms.GroupBox
+# $radioButtonCompresion = New-Object System.Windows.Forms.RadioButton
+# $radioButtonCompresion.Text = "Comprime"
+# $radioButtonCompresion.Checked = $true
+# $radioButtonRedimension = New-Object System.Windows.Forms.RadioButton
+# $radioButtonRedimension.Text = "Resize+Compr"
+# $radioButtonCompresion.Location = New-Object System.Drawing.Point(10,10)
+# $radioButtonRedimension.Location = New-Object System.Drawing.Point(115,10)
+
+$labelSpeed = New-Object System.Windows.Forms.Label
+$labelSpeed.Text = "Desired Speed:"
+$labelSpeed.Location = New-Object System.Drawing.Point(10,10)
+$labelSpeed.Size = New-Object System.Drawing.Size(100,20)
+
+$textboxSpeed = New-Object System.Windows.Forms.TextBox
+$textboxSpeed.Text = "1.3"
+$textboxSpeed.Location = New-Object System.Drawing.Point(120,10)
+$textboxSpeed.Size = New-Object System.Drawing.Size(80,20)
+
+$groupbox3.Controls.AddRange($labelSpeed, $textboxSpeed)
+$groupbox3.Location = New-Object System.Drawing.Point(10,80)
+$groupbox3.Size = New-Object System.Drawing.Size(260,40)
+#----------------------------------------------------------------------
+
+$labelMensaje = New-Object System.Windows.Forms.Label
+$labelMensaje.Location = New-Object System.Drawing.Point(10,140)
+$labelMensaje.Size = New-Object System.Drawing.Size(60,20)
+$labelMensaje.Text = "....."
 
 $miBoton = New-Object System.Windows.Forms.Button
 $miBoton.Location = New-Object System.Drawing.Point(10,110)
-$miBoton.Size = New-Object System.Drawing.Size(100,20)
+$miBoton.Size = New-Object System.Drawing.Size(100,30)
 $miBoton.Text = "Procesar"
 $miBoton.Add_Click({
-    $inputFolder = $miCajaTexto.Text
+    $inputFolder = $textboxFolderPath.Text
     $outputFolder = Join-Path $inputFolder "salida\"
-
+    
     if(!(Test-Path $outputFolder)){
         New-Item -ItemType Directory -Path $outputFolder | Out-Null
     }
+
 
     Get-ChildItem -Path $inputFolder -Filter *.mp4 | ForEach-Object{
         $inputFile = $_.FullName
@@ -43,18 +86,39 @@ $miBoton.Add_Click({
 
         Write-Output "Procesando file: $inputFile"
 
-        if($radioButtonCompresion.Checked){
-            ffmpeg -i $inputFile -c:v libx265 -crf 28 -preset medium -c:a copy $outputFile
-        }else {
-            ffmpeg -i $inputFile -c:v libx265 -crf 28 -preset medium -vf "scale=1280:720" -c:a copy $outputFile
-        }
+        $rapidez = $textboxSpeed.Text
+        # ffmpeg -i $inputFile -c:v libx265 -crf 28 -preset medium -vf "scale=1280:720" -c:a copy $outputFile
+        # $ffmpegCommand = "ffmpeg -i $inputFile -vf `"scale=1280:720,setpts=PTS/1.4`" -af `"atempo=1.4`" -c:v libx265 -crf 28 -preset medium -c:a aac -b:a 128k $outputFile"
+        $ffmpegCommand = "ffmpeg -i $inputFile -vf `"scale=1280:720,setpts=PTS/$rapidez`" -af `"atempo=$rapidez`" -c:v libx265 -crf 28 -preset medium -c:a aac -b:a 128k $outputFile"
 
-        [System.Media.SystemSounds]::Exclamation.Play()
+        if($checkboxCompress.Checked -eq $false){
+            $ffmpegCommand = $ffmpegCommand.Replace(" -crf 28 -preset medium", "")
+        }
+        if($checkboxResize.Checked -eq $false -and $checkboxSpeed.Checked -eq $false){
+            $ffmpegCommand = $ffmpegCommand.Replace(" -vf `"scale=1280:720,setpts=PTS/1.4`" -af `"atempo=1.4`"", "")
+        }
+        elseif($checkboxResize.Checked -eq $false){
+            $ffmpegCommand = $ffmpegCommand.Replace("scale=1280:720,", "")
+        }
+        elseif($checkboxSpeed.Checked -eq $false){
+            $ffmpegCommand = $ffmpegCommand.Replace(",setpts=PTS/1.4`" -af `"atempo=1.4", "")
+        }
+        
+        Invoke-Expression $ffmpegCommand
     }
+    [System.Media.SystemSounds]::Exclamation.Play()
+    # Show popup message
+    # [System.Windows.Forms.MessageBox]::Show($ffmpegCommand, "Información", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    $ffmpegCommand | Set-Clipboard
+    $labelMensaje.Text = "termino..."
 })
 
-$miFormulario.Controls.Add($miCajaTexto)
-$miFormulario.Controls.Add($miLabel)
+$miBoton.Location = New-Object System.Drawing.Point(160,130)
+
+$miFormulario.Controls.Add($labelFolderPath)
+$miFormulario.Controls.Add($textboxFolderPath)
+$miFormulario.Controls.Add($groupbox2)
+$miFormulario.Controls.Add($groupbox3)
+$miFormulario.Controls.Add($labelMensaje)
 $miFormulario.Controls.Add($miBoton)
-$miFormulario.Controls.Add($miGroupBox)
 $miFormulario.ShowDialog()
